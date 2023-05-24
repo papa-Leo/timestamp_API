187c2ce2ed65852c7f12afedc05b847b261e8695
papa-Leo <leandroconsani21@gmail.com>
2023-05-24 22:44:56 +0200

    forgot port again

--- index.js
+++ index.js
@@ -47,6 +47,6 @@ app.get('/api/:date?', (req, res) => {
 });
 
 // listen for requests :)
-var listener = app.listen(/*process.env.PORT*/3000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('🎉 Your app is listening on port ' + listener.address().port + ' 🎉');
 });
